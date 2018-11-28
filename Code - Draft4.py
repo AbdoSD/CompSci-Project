f92d4e3398dddf3a17d3d98c8bbef31a94163e24
AbdoSD <40603577+AbdoSD@users.noreply.github.com>
2018-11-28 18:49:19 +0000

    Basic game screen working
    Player works, can move and jumps,
    basic platforms at the start which player can jump on and off.

--- Code - Draft4.py
+++ Code - Draft4.py
@@ -1,482 +1,199 @@
-import pygame as pg
-
+import pygame 
+import math
 import random
 
-import math
-
-from settings import *
-
-from sprites2 import *
-
-from os import path
 
 
+#colours to be used in game
+Green = (50,205 ,50)
+Ivory = (255,255,240)
+Blue = (0,0,255)
+Red = (255,0,0)
+Purple = (255,0,255)
+Grey = (190,190,190)
+Black = (0,0,0)
+White = (255, 255, 255)
+Yellow = (255, 255, 0)
+
+#screen width and height 
+WIDTH = 540
+HEIGHT = 720
 
 
 
-
-
-class Game:
-
-        def __init__(self):
-
-                #initialise game window, clock and the font for any text on screen
-
-                pg.init()
-
-                pg.mixer.init()
-
-                self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-
-                pg.display.set_caption(TITLE)
-
-                self.clock = pg.time.Clock()
-
-                self.running = True
-
-                self.font_name = pg.font.match_font(FONT_NAME)
-
-                self.load_data()
-
-                self.player = None
+class Player(pygame.sprite.Sprite):
+        def __init__(self, platforms_group):
+                super().__init__()
+                
+                #set height and width as local variables, fill the colour of the image.
+                width = 50
+                height = 50
+                self.image = pygame.Surface([width,height])
+                self.image.fill(Green)
+                self.rect = self.image.get_rect()
+                 #speed of the player. Change in its x direction or change
+                # in its  y direction
+                self.change_x = 0
+                self.change_y = 0
+                # gains the platform as an attribute so it can gain the platforms attributes and methods
+                self.platforms_group = platforms_group
 
                 
-
-
-
-        def load_data(self):
-
-                self.dir = path.dirname(__file__)
-
-               
-
-                with open(path.join(self.dir, HS_FILE), 'r') as f:
-
-                        #try and except runs some code and if an error becomes present it runs some other code to run
-
-                        try:
-
-                                self.highscore = int(f.read())
-
-                        except:
-
-                                self.highscore = 0
-
-                
-
-               
-
-
-
-        def new(self):
-
-                # Sets the score at the start and defines the players and platforns importing their classes
-
-                #from the sprite file.
-
-                self.score = 0 
-
-                self.all_sprites = pg.sprite.LayeredUpdates()
-
-                self.platforms = pg.sprite.Group()
-
-                self.powerups = pg.sprite.Group()
-
-                self.meteorites = pg.sprite.Group()
-
-                self.fireballs = pg.sprite.Group()
-
-                self.enemies = pg.sprite.Group()
-
-                self.stars = pg.sprite.Group()
-
-                self.player = Player(self)
-
-                for plat in PLATFORM_LIST:
-
-                       Platform(self, *plat)
-
-                self.enemy_timer = 0
-
-                for i in range (6):
-
-                        s = Star(self)
-
-                        s.rect.y += 500
-
-                self.run()
-
-                
-
-                
-
-                
-
-
-
-        def run(self):
-
-                #game loop
-
-                self.playing = True
-
-                while self.playing:
-
-                        self.clock.tick(FPS)
-
-                        self.events()
-
-                        self.update()
-
-                        self.draw()
-
-                       
-
-               
-
-
-
         def update(self):
 
-                 #Updates including when the player jumps above and lands on a platform
+                #Gravity, defined below
+                self.gravity()
+               #To move left or right
+                self.rect.x += self.change_x
+                #To move up or down
+                self.rect.y += self.change_y
+                #Check to see if player collides with something
+                #Resets player position based on the top or bottom of the object
+                if self.change_y > 0:
+                        hits= pygame.sprite.spritecollide(self, self.platforms_group, False)
+                        #If player hits something take the players y coordinate and set it to the top
+                        # of the object it has collided with
+                        #set y velocity to 0 so the player stops moving
+                        if hits:
+                                self.rect.bottom = hits[0].rect.top
+                                self.change_y = 0
+                                
+                #wraps around the top and bottom of screen
+                if self.rect.x > WIDTH:
+                        self.rect.x= 0
+                if self.rect.x < 0:
+                        self.rect.x = WIDTH
 
-                self.all_sprites.update()
+        def gravity(self):
+                #Effects of gravity
+                if self.change_y == 0:
+                        self.change_y = 1
+                else:
+                        self.change_y += 0.25
 
-                #spawn an enemy
-
-                now = pg.time.get_ticks()
-
-                if now - self.enemy_timer > 2500 + random.choice([-1000, -500, 0, 500, 1000]):
-
-                        self.enemy_timer = now
-
-                        Enemy(self)
-
-                # hit enemies
-
-                enemy_hits = pg.sprite.spritecollide(self.player, self.enemies, False)
-
-                if enemy_hits:
-
-                        self.playing = False
-
-                # check if player hits a platform - only if falling
-
-                if self.player.vel.y > 0:
-
-                         hits = pg.sprite.spritecollide(self.player, self.platforms, False)
-
-                         if hits:
-
-                                 lowest = hits[0]
-
-                                 for hit in hits:
-
-                                         if hit.rect.bottom > lowest.rect.bottom:
-
-                                                 lowest = hit
-
-                                 if self.player.pos.x < lowest.rect.right + 10 and \
-                                    self.player.pos.x > lowest.rect.left - 10:
-
-                                         if self.player.pos.y < lowest.rect.bottom:
-
-                                                 self.player.pos.y = lowest.rect.top
-
-                                                 self.player.vel.y = 0
-
-                                                 self.player.jumping = False
-
-
-
-                # check to see if a fireball hit an enemy
-
-                hits = pg.sprite.groupcollide(self.enemies, self.fireballs, True, True)
-
+        def jump(self):
+                #check below to see if there is anything to jump from then shift back up 
+                self.rect.y +=2
+                hits = pygame.sprite.spritecollide(self, self.platforms_group, False)
+                self.rect.y -=2
+                #Set the speed upwards if its ok to jump
                 if hits:
-
-                        self.score +=5
-
-
-
-                # if the player reaches the top 1/3 of the screen platforms start to disappear and points can be
-
-                # gained this way
-
-                if self.player.rect.top <= HEIGHT / 3:
-
-                        if random.randrange(100) < 15:
-
-                                Star(self)
-
-                        self.player.pos.y += max(abs(self.player.vel.y), 2)
-
-                        for star in self.stars:
-
-                                star.rect.y += max(abs(self.player.vel.y / 2), 2)
-
-                        for enemy in self.enemies:
-
-                                 enemy.rect.y += max(abs(self.player.vel.y),2)
-
-                        for plat in self.platforms:
-
-                                 plat.rect.y += max(abs(self.player.vel.y), 2)
-
-                                 if plat.rect.top >= HEIGHT:
-
-                                         plat.kill()
-
-                                         self.score += 20
-
-
-
-                # If player hits a powerup
-
-                power_hits = pg.sprite.spritecollide(self.player, self.powerups, True)
-
-                for power in power_hits:
-
-                         if power.type == 'boost' :
-
-                                 self.player.vel.y = -BOOST_POWER
-
-                                 self.player.jumping = False
-
-
-
-                # If player hits a meteorite
-
-               
-
-
-
-
-
-                #Die
-
-                if self.player.rect.bottom > HEIGHT:
-
-                         for sprite in self.all_sprites:
-
-                                 sprite.rect.y -= max(self.player.vel.y, 10)
-
-                                 if sprite.rect.bottom < 0:
-
-                                         sprite.kill()
-
-                if len(self.platforms) == 0:
-
-                         self.playing = False
-
-
-
-                       
-
-                                 
-
-                # spawn new platforms to keep average number
-
-                while len(self.platforms) < 6:
-
-                         width = random.randrange(50, 100)
-
-                         Platform(self,random.randrange(0, WIDTH - width),
-
-                                              random.randrange(-75, - 30),
-
-                                              width, 25)
-
-                         
-
-                         
-
-        def events(self):
-
-                #Game loop - events
-
-                 for event in pg.event.get():
-
-                        if event.type == pg.QUIT:
-
-                                if self.playing:
-
-                                        self.playing = False
-
-                                self.running = False
-
-                        if event.type ==pg.KEYDOWN:
-
-                                if event.key == pg.K_SPACE:
-
-                                        self.player.jump()
-
-                        if event.type ==pg.KEYUP:
-
-                                if event.key == pg.K_SPACE:
-
-                                        self.player.jump_cut()
-
-                        if event.type == pg.KEYDOWN:
-
-                                if event.key == pg.K_UP:
-
-                                        self.player.shoot()
-
-                        if event.type == pg.KEYDOWN:
-
-                                if event.key == pg.K_DOWN:
-
-                                        self.player.shoot()
-
-
-
-        def draw(self):
-
-                #Game loop - draw
-
-                self. screen.fill(Blue)
-
-                self.all_sprites.draw(self.screen)
-
-                self.draw_text(str(self.score), 22, White, WIDTH / 2, 15)
-
-        
-
-                pg.display.flip()
+                        self.change_y = -8
+                        
+        #Player controlled movement
+        def move_left(self):
+                 self.change_x = -5
+
+        def move_right(self):
+                 self.change_x = 5
+
+        def stop(self):
+                self.change_x = 0
+
+        #Die. If player hits the bottom of the screen, 
+        def die(self):
+                if self.rect.bottom > HEIGHT:
+                        self.kill()
+                        done = True
+                # if there are no platforms
+                if len(self.platforms_group) == 0:
+                        done = True
 
                 
 
+class Platform(pygame.sprite.Sprite):
+        def __init__(self,x,y,w,h):
+                super().__init__()
+                self.image = pygame.Surface([100, 25])
+                self.image.fill(White)
+                self.rect = self.image.get_rect()
+                self.rect.x = x
+                self.rect.y = y 
                 
-
-               
-
-        def show_start_screen(self):
-
-                self.screen.fill(Blue)
-
-                self.draw_text(TITLE, 48, White, WIDTH / 2, HEIGHT / 4)
-
-                self.draw_text("Left and Right arrows to move", 22, White, WIDTH / 2, HEIGHT / 2)
-
-                self.draw_text("Up and down arrows to shoot", 22, White, WIDTH / 2, HEIGHT * 55/100)
-
-                self.draw_text("Space to jump", 22, White, WIDTH / 2, HEIGHT * 3/5)
-
-                self.draw_text("Press a key to play", 22, White, WIDTH / 2, HEIGHT * 3/4)
-
-                self.draw_text("High Score: " + str(self.highscore), 22, White, WIDTH / 2, 15)
-
-                pg.display.flip()
-
-                self.wait_for_key( )
-
                 
-
-                
-
-                
-
-
-
-        def show_go_screen(self):
-
-                # game over or continue
-
-                 if not self.running:
-
-                         return
-
-                 self.screen.fill(Blue)
-
-                 self.draw_text("Game Over", 48, White, WIDTH / 2, HEIGHT / 4)
-
-                 self.draw_text("Score: " + str(self.score), 22, White, WIDTH / 2, HEIGHT / 2)
-
-                 self.draw_text("Press a key to play again", 22, White, WIDTH / 2, HEIGHT * 3/4)
-
-                 if self.score > self.highscore:
-
-                         self.highscore =  self.score
-
-                         self.draw_text("NEW HIGH SCORE!", 22, White, WIDTH / 2, HEIGHT / 2 + 40)
-
-                         with open(path.join(self.dir, HS_FILE), 'w') as f:
-
-                                 f.write(str(self.score))
-
-                 else:
-
-                        self.draw_text("High Score: " + str(self.highscore), 22, White, WIDTH / 2, HEIGHT / 2 + 40)
-
-                   
-
-                 pg.display.flip()
-
-                 self.wait_for_key()
-
+       
+                        
                  
+#initilaise pygame
+pygame.init()
 
-                 
-
-
-
-        def wait_for_key(self):
-
-                 waiting = True
-
-                 while waiting:
-
-                         self.clock.tick(FPS)
-
-                         for event in pg.event.get():
-
-                                 if event.type == pg.QUIT:
-
-                                         waiting = False
-
-                                         self.running = False
-
-                                 if event.type == pg.KEYUP: 
-
-                                          waiting = False
+size = [WIDTH, HEIGHT]
+screen = pygame.display.set_mode(size)
 
 
+#set title of the window
+pygame.display.set_caption('Space Hopper')
 
-        def draw_text(self, text, size, color, x, y):
-
-                 font = pg.font.Font(self.font_name, size)
-
-                 text_surface = font.render(text, True, color)
-
-                 text_rect = text_surface.get_rect()
-
-                 text_rect.midtop = (x, y)
-
-                 self.screen.blit(text_surface, text_rect)
-
-               
+#list to hold the spritrs
+all_sprites_list = pygame.sprite.Group()
+#create the sprites
+#platform = Platform()
+all_platforms_group = pygame.sprite.Group()
+p1 = Platform(300, HEIGHT - 100, 100,50)
+all_sprites_list.add(p1)
+all_platforms_group.add(p1)
+p2 = Platform(180, HEIGHT -  220, 100, 50)
+all_sprites_list.add(p2)
+all_platforms_group.add(p2)
+p3 = Platform(380, HEIGHT -  350, 100, 50)
+all_sprites_list.add(p3)
+all_platforms_group.add(p3)
+p4 = Platform(200, HEIGHT -  450, 100, 50)
+all_sprites_list.add(p4)
+all_platforms_group.add(p4)
+p5 = Platform(330, HEIGHT -  600, 100, 50)
+all_sprites_list.add(p5)
+all_platforms_group.add(p5)
 
 
 
+player = Player(all_platforms_group)
+
+#sets the player postion x and y cooridnates and adds it to sprite list
+player.rect.x = 300
+player.rect.y = HEIGHT - 100
+
+all_sprites_list.add(player)
+#loop until the player clicks the close button
+done = False
+#manage how fast the screen updates
+clock = pygame.time.Clock()
 
 
+#-------MAIN PROGRAM LOOP--------#
+while not done:
+         for event in pygame.event.get():
+                 if event.type == pygame.QUIT:
+                        done = True
+
+                # When holding down a key      
+                 keys = pygame.key.get_pressed()
+                 if keys [pygame.K_LEFT]:
+                         player.move_left()
+                 if keys [pygame.K_RIGHT]:
+                         player.move_right()
+                 if keys [pygame.K_SPACE]:
+                         player.jump()
+
+                 if event.type == pygame.KEYUP:
+                         if event.key == pygame.K_LEFT and player.change_x < 0:
+                                 player.stop()
+                         if event .key == pygame.K_RIGHT and player .change_x >0:
+                                 player.stop()
+                
+        #DRAW TEXT CODE
+         screen.fill(Blue)
+         all_sprites_list.draw(screen)
+
+         #Update the player
+         all_sprites_list.update()
+
+         
+         #limit to 60 frames per second of screen update
+         clock.tick(60)
+        #updates the screen with any changes
+         pygame.display.flip()
+
+pygame.quit()
 
 
-                 
-
-
-
-g = Game()
-
-g.show_start_screen()
-
-while g.running:
-
-        g.new()
-
-        g.show_go_screen()
-
-
-
-pg.quit()
